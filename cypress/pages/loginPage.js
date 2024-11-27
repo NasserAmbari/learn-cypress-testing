9a1c8976ac91757e4b9fca57861d2e7e3988f9f9
user <user@Nasser-Minergo.local>
2024-11-27 18:09:29 +0800

    Add new POM in folder pages

---
+++ cypress/pages/loginPage.js
@@ -0,0 +1,67 @@
+class LoginPage {
+    visit() {
+        cy.visit('/auth/login');
+    }
+
+    isUsernameVisible(){
+        cy.get('input[name="username"]').should('be.visible');
+    }
+
+    isPasswordVisible(){
+        cy.get('input[name="password"]').should('be.visible');
+    }
+
+    isLoginBtnVisible(){
+        cy.get('button[type="submit"]').should('be.visible').and('contain', 'Login');
+    }
+
+    isDashboard(){
+        cy.url().should('include', '/dashboard');
+        cy.get('.oxd-topbar-header-breadcrumb').should('contain', 'Dashboard');
+    }
+  
+    enterUsername(username) {
+        cy.get('input[name="username"]').type(username);
+    }
+  
+    enterPassword(password) {
+        cy.get('input[name="password"]').type(password);
+    }
+  
+    clickLogin() {
+        cy.get('button[type="submit"]').click();
+    }
+
+    clickForgotPass(){
+        cy.contains('Forgot your password?').click();
+    }
+  
+    getErrorMessage() {
+        return cy.get('.oxd-alert-content').should('be.visible').and('contain', 'Invalid credentials');
+    }
+
+    getErrorMessageRequired(){
+        cy.get('.oxd-input-group__message')
+        .should('have.length', 2) 
+        .and('contain', 'Required');
+    }
+
+    clickLikedin(){
+        cy.get('a[href="https://www.linkedin.com/company/orangehrm/mycompany/"]').should('be.visible').click();
+    }
+
+    clickFacebook(){
+        cy.get('a[href="https://www.facebook.com/OrangeHRM/"]').should('be.visible').click();
+    }
+
+    clickTwitter(){
+        cy.get('a[href="https://twitter.com/orangehrm?lang=en"]').should('be.visible').click();
+    }
+
+    clickYoutube(){
+        cy.get('a[href="https://www.youtube.com/c/OrangeHRMInc"]').should('be.visible').click();
+    }
+  }
+  
+  module.exports = LoginPage;
+  
