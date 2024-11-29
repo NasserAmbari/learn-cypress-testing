ccd95686a909f93a8c8dfb8fc677a2d579bb044c
user <user@Nasser-Minergo.local>
2024-11-30 00:15:07 +0800

    Add new Forgot Password Page

--- cypress/pages/forgotPasswordPage.js
+++ cypress/pages/forgotPasswordPage.js
@@ -1,10 +1,26 @@
 class ForgotPasswordPage {
     visit(){
-        cy.visit('/auth/requestPasswordReset');
+        cy.visit('/auth/requestPasswordResetCode');
     }
 
     isForgotPassword(){
-        cy.url().should('include', '/auth/requestPasswordReset');
+        cy.url().should('include', '/auth/requestPasswordResetCode');
+    }
+
+    enterUsername(username) {
+        cy.get('input[name="username"]').type(username);
+    }
+
+    clickResetButton() {
+        cy.get('button[type="submit"]').click();
+    }
+
+    clickCancelButton(){
+        cy.get('.orangehrm-forgot-password-button--cancel').click();
+    }
+
+    verifySuccessMessage() {
+        cy.contains('Reset Password link sent successfully').should('be.visible'); // Verifikasi pesan sukses
     }
 }
 
